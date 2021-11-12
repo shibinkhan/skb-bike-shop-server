b001dd0a654412ff5e9a2b0122c22dc2d3af4ce0
shibinkhan <khanshibin500@gmail.com>
2021-11-13 02:02:31 +0600

    2nd commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const cors = require('cors');
 const express = require('express');
 const { MongoClient } = require('mongodb');
-// const ObjectId = require('mongodb').ObjectId;
+const ObjectId = require('mongodb').ObjectId;
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -20,9 +20,132 @@ async function server() {
         await client.connect();
         console.log('Database is Connected.');
 
-        const database = client.db('Doctors-Portal')
-        // const appointmentsCollection = database.collection('appointments');
-        // const usersCollection = database.collection('users');
+        const database = client.db('SKB-Bike-Shop')
+        const bikesCollection = database.collection('bikes');
+        const usersCollection = database.collection('users');
+        const ordersCollection = database.collection('orders');
+        const reviewsCollection = database.collection('reviews');
+
+        // bikes : POST a bike
+        app.post('/bikes', async (req, res) => {
+            const bike = req.body;
+            console.log(bike);
+            const bikeAddResult = await bikesCollection.insertOne(bike);
+            res.json(bikeAddResult);
+        });
+
+        // bikes : GET all
+        app.get('/bikes', async (req, res) => {
+            const cursor = bikesCollection.find({});
+            const bikeAddResult = await cursor.toArray();
+            res.send(bikeAddResult);
+        });
+
+        // bikes : GET six orders
+        app.get('/bikes/six', async (req, res) => {
+            const cursor = bikesCollection.find();
+            const bikeAddResult = await cursor.limit(6).toArray();
+            res.send(bikeAddResult);
+        });
+
+        // bikes : GET a bike
+        app.get('/bikes/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const bike = await bikesCollection.findOne(query);
+            res.json(bike);
+        });
+
+        // bikes : DELETE a bike
+        app.delete('/bikes/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const deleteResult = await bikesCollection.deleteOne(query);
+            res.json(deleteResult);
+        });
+
+        // orders : POST an order
+        app.post('/orders', async (req, res) => {
+            const order = req.body;
+            const orderResult = await ordersCollection.insertOne(order);
+            res.json(orderResult);
+        });
+        
+        // orders : GET all orders
+        app.get('/orders', async (req, res) => {
+            const cursor = ordersCollection.find({});
+            const orderResult = await cursor.toArray();
+            res.send(orderResult);
+        });
+        
+        // orders : DELETE an order
+        app.delete('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const deleteResult = await ordersCollection.deleteOne(query);
+            res.json(deleteResult);
+        });
+        
+        // reviews : POST a review
+        app.post('/reviews', async (req, res) => {
+            const review = req.body;
+            const reviewResult = await reviewsCollection.insertOne(review);
+            res.json(reviewResult);
+        });
+
+        // reviews : GET all reviews
+        app.get('/reviews', async (req, res) => {
+            const cursor = reviewsCollection.find({});
+            const reviewResult = await cursor.toArray();
+            res.send(reviewResult);
+        });
+
+        // users : POST a user
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const userResult = await usersCollection.insertOne(user);
+            res.json(userResult);
+        });
+
+        // users : PUT/Update
+        app.put('/users', async (req, res) => {
+            const user = req.body;
+            // console.log('put', user);
+            const filter = { email: user.email };
+            const options = { upsert: true };
+            const updateDoc = { $set: user };
+            const userResult = await usersCollection.updateOne(filter, updateDoc, options);
+            res.json(userResult);
+        });
+
+        // admin : PUT/Update
+        app.put('/users/admin', async (req, res) => {
+            const user = req.body;
+            console.log('put', user);
+            const filter = { email: user.email };
+            const updateDoc = { $set: { role: 'admin' } };
+            const userResult = await usersCollection.updateOne(filter, updateDoc);
+            res.json(userResult);
+        });
+
+        // admin : GET 
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            // console.log('put', user);
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            let isAdmin = false;
+            if (user?.role === 'admin') {
+                isAdmin = true;
+            };
+            res.json({ admin: isAdmin });
+        });
+
+
+
+
+
+        
 
     }
     finally {
